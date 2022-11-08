8db4ef3320d59a99f8e87d2e81375e9e2f5501c1
ruslan <ruslanburalkiev8@gmail.com>
2022-11-08 21:29:39 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+//////1
+
+// let count = 0;
+// function recursion() {
+//     count ++;
+//     console.log(count, 'array');
+//     if(count === 3) {
+//         return
+//     } else {
+//         return recursion()
+//     }
+// }
+// recursion();
+
+////2
+
+// let arr = [4,5,6]
+// arr.push(7,8,9,10)
+// arr.unshift(1,2,3)
+// console.log(arr);
+
+/////3
+
+// let arr1 = [1,2,3,4,5];
+// for (let i = 0; i<arr1.length;i++);
+//     console.log(arr1.pop());
+
+
+/////4
+
+// function countchar(){
+//     let count = 'loremipsumdolor'
+//     console.log(count.match(/o/g));
+// }
+// countchar();
